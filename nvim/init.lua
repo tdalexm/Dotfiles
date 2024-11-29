e75fbf9ea53402b6a7a8c96bb54d1935eacd743b
tdalexm <168939701+tdalexm@users.noreply.github.com>
2024-11-29 10:50:54 +0100

    Update init.lua with formating

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,8 @@
 require("config.lazy")
 vim.opt.timeoutlen = 1000
 vim.opt.ttimeoutlen = 0
+vim.opt.tabstop = 4 -- Width of a tab character
+vim.opt.shiftwidth = 4 -- Width for autoindent
+vim.opt.expandtab = false -- Use actual tabs instead of spaces
+vim.opt.softtabstop = 4  -- install css-ls from mason
+ 
